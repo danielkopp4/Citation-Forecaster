ddcfe17dc08540d993034d0da4ecf9d2688049d1
danielkopp4 <dan.r.kopp@gmail.com>
2023-01-26 13:53:00 -0500

    new branch 'data_collection'

--- data_extraction/src/downloader.py
+++ data_extraction/src/downloader.py
@@ -1,4 +1,9 @@
 
 
-if __name__ == "__main__":
+# when called downloads and parses the dataset
+# for future use in the dataset_api
+def download_data():
     pass
+
+if __name__ == "__main__":
+    download_data()
