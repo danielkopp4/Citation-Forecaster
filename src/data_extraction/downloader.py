c4c8a7d5fdd42cea1fcff124fd6c94d6d14ddc06
balajv <balajv@rpi.edu>
2023-01-28 12:04:49 -0500

    added functionality for arXiv data ingestion - need to test

--- src/data_extraction/downloader.py
+++ src/data_extraction/downloader.py
@@ -1,11 +1,25 @@
 from src.shared.utils import load_params
 import sys
+from kaggle.api.kaggle_api_extended import KaggleApi
+from zipfile import ZipFile
+api = KaggleApi()
+api.authenticate()
 
 
 # when called downloads and parses the dataset
 # for future use in the dataset_api
 def download_data(params: dict):
+    api.dataset_download_file(params["data_set"], params["json_file"])
+    zipFileName = params["json_file"] + ".zip"
+    zf = ZipFile(zipFileName)
+    zf.extractall()
+    zf.close()
+    loc = os.path.join(params['data_folder'], params['processed_name'])
+
+    if not os.path.exists(loc):
+        os.mkdir(loc)
     pass
+
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
