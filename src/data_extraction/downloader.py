ed4495e88e3a66c633d0458b13395aca0593d997
balajv <balajv@rpi.edu>
2023-02-03 17:26:38 -0500

    made code concise and import is now streamlined

--- src/data_extraction/downloader.py
+++ src/data_extraction/downloader.py
@@ -2,10 +2,7 @@ import os
 from src.shared.utils import load_params
 import sys
 import opendatasets as od
-from kaggle.api.kaggle_api_extended import KaggleApi
-from zipfile import ZipFile
-api = KaggleApi()
-api.authenticate()
+from scholarly import scholarly
 
 
 # when called downloads and parses the dataset
@@ -14,17 +11,27 @@ def download_data(params: dict):
     
     # download the data
     dataset_link = params["data_link"]
-    od.download(dataset_link)
-    loc = os.path.join(params['data_folder'], params['processed_name'])
-    newpath = params['data_folder']
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)
-    
-    os.rename(params["initial_data_location"], loc)
+    loc = os.path.join(params['data_folder'], params['dataset_folder'])
+    od.download(dataset_link, data_dir=loc)
+    # newpath = params['data_folder']
+    # if not os.path.exists(newpath):
+    #     os.makedirs(newpath)
 
-    if not os.path.exists(loc):
-        os.mkdir(loc)
-    pass
+    # newpath2 = params['dataset_folder']
+    # if not os.path.exists(newpath2):
+    #     os.makedirs(newpath2)
+
+
+    # loc = os.path.join(params['dataset_folder'], params['processed_name'])
+    
+    # os.rename(params["initial_data_location"], loc)
+
+    # if not os.path.exists(loc):
+    #     os.mkdir(loc)
+
+
+
+
 
 
 if __name__ == "__main__":
