55aff449a96aa9f370832dae72dcf0fd12f60ced
balajv <balajv@rpi.edu>
2023-02-10 18:08:40 -0500

    added testing for scholarly api

--- src/data_extraction/downloader.py
+++ src/data_extraction/downloader.py
@@ -4,7 +4,6 @@ import sys
 import opendatasets as od
 from scholarly import scholarly
 
-
 # when called downloads and parses the dataset
 # for future use in the dataset_api
 def download_data(params: dict):
@@ -13,22 +12,8 @@ def download_data(params: dict):
     dataset_link = params["data_link"]
     loc = os.path.join(params['data_folder'], params['dataset_folder'])
     od.download(dataset_link, data_dir=loc)
-    # newpath = params['data_folder']
-    # if not os.path.exists(newpath):
-    #     os.makedirs(newpath)
 
-    # newpath2 = params['dataset_folder']
-    # if not os.path.exists(newpath2):
-    #     os.makedirs(newpath2)
-
-
-    # loc = os.path.join(params['dataset_folder'], params['processed_name'])
-    
-    # os.rename(params["initial_data_location"], loc)
-
-    # if not os.path.exists(loc):
-    #     os.mkdir(loc)
-
+    search_query = scholarly.search_author('Steven A Cholewiak')
 
 
 
