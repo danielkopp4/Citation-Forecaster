04248efe554890217b6c74dc5399ebd0eda16ead
danielkopp4 <dan.r.kopp@gmail.com>
2023-04-01 00:48:29 -0400

    downloading citations

--- src/data_extraction/downloader.py
+++ src/data_extraction/downloader.py
@@ -4,8 +4,6 @@ import numpy as np
 from fp.fp import FreeProxy
 import time
 from typing import List
-from requests.packages.urllib3.exceptions import MaxRetryError
-from requests.packages.urllib3.exceptions import ProxyError as urllib3_ProxyError
 from requests.exceptions import ConnectionError
 import traceback
 from src.shared.utils import load_params
@@ -15,12 +13,10 @@ import sys
 import shutil
 from typing import List
 import json
-import random
-import socket
-import struct
+from datetime import timedelta
 
-max_cutoff = 20
-batch = 2
+max_cutoff = np.inf
+batch = 100
 interval = 2
 
 
@@ -59,6 +55,11 @@ def save_complete(params: dict, complete: np.ndarray):
 def get_incomplete(complete: np.ndarray):
     return np.arange(len(complete))[complete == 0]
 
+def iterate_exp_average(current_val: float, moving_average: float, beta_1: float, beta_2: float, iters: int) -> float:
+    movement = np.log(beta_1) / np.log(beta_2)
+    adj_beta = -np.power(beta_2, iters + movement) + beta_1
+    return adj_beta * moving_average + (1 - adj_beta) * current_val
+
 # when called downloads and parses the dataset
 # for future use in the dataset_api
 def download_data(params: dict):
@@ -82,14 +83,33 @@ def download_data(params: dict):
     incomplete = get_incomplete(complete)
 
     id = 0
+    rate = 0
+    prev_time = time.time()
+    prev_n_remaining = np.sum([1 if x == 0 else 0 for x in complete])
+    iters = 0
+
     while np.any(complete == False):
         if id < len(dois):
             start_thread(incomplete[id:id+batch], dois, complete)
             id += batch
         time.sleep(interval)
-        print("\033[Kn remaining: {}".format(np.sum([1 if x == 0 else 0 for x in complete])), end="\r")
+
+        delta = time.time() - prev_time
+        prev_time = time.time()
+        n_remaining = np.sum([1 if x == 0 else 0 for x in complete])
+        delta_n = prev_n_remaining - n_remaining
+        prev_n_remaining = n_remaining
+        current_rate = delta_n / delta
+
+        rate = iterate_exp_average(current_rate, rate, 0.97, 0.5, iters)
+        iters += 1
+        time_remaining = n_remaining / rate if rate > 1E-5 else np.inf
+        time_remaining = timedelta(seconds=int(time_remaining)) if not np.isinf(time_remaining) else "inf"
+
+        print("\033[Kn remaining: {} | {:0.03f} req/s | {} seconds".format(n_remaining, rate, time_remaining), end="\r")
         save_complete(params, complete)
 
+    save_complete(params, complete)
     print("req per second",  len(dois) / (time.time() - prev_time))
 
 
@@ -103,14 +123,12 @@ def get_citation(ids: List[int], dois: List[str], complete: np.ndarray) -> None:
     # API_CALL = "https://opencitations.net/index/api/v1/citation-count/{}".format(doi)
 
     
-    # print(doi)
     for id in ids:
         curDoi = dois[id]
         if curDoi == None:
             complete[id] = -100
             continue
 
-        # print(complete)
         
         url = f"http://api.crossref.org/works/{curDoi}"
 
@@ -130,31 +148,17 @@ def get_citation(ids: List[int], dois: List[str], complete: np.ndarray) -> None:
             """.strip().split("\n")]
             headers = {x: y for x,y in headers}
 
-
-
-
-            # citationNumber = get(url, proxies=proxies, headers=headers)
-            # print(citationNumber.status_code)
-            # print(url)
-            # print("got rate limited")
-            # print(citationNumber.raw)
-            
             try:
-                # citationNumber = get(url, proxies=proxies, timeout=10)
                 citationNumber = get(url, headers=headers, proxies=proxies, timeout=100)
 
                 if citationNumber.status_code == 200:
                     success = True
             except ConnectionError:
-                # print("received connection error")
-                # traceback.print_exc(file=sys.stdout)
-                # complete[id+i] = 1
                 pass
             except Exception:
                 print("received unhandled error")
                 print(url)
                 traceback.print_exc(file=sys.stdout)
-                # complete[id] = 0
                 sys.exit()
 
             if not success:
@@ -165,32 +169,20 @@ def get_citation(ids: List[int], dois: List[str], complete: np.ndarray) -> None:
 
         try:
             count = int(citationNumber.json()['message']['reference-count'])
-            complete[id] = count + 1
-        
-
-        # except ConnectionError:
-        #     print("received connection error")
-        #     traceback.print_exc(file=sys.stdout)
-        #     complete[id+i] = True
-        #     continue
+            complete[id] = count + 1 # important -> subtract by 1 when loading
         except Exception:
             print("received unhandled error")
             print(citationNumber.text)
             print(url)
             traceback.print_exc(file=sys.stdout)
-            # complete[id] = 0
         
 
 def start_thread(ids, dois, complete):
-    # print("starting thread", id, doi)
-    Thread(target=get_citation, args=(ids, dois, complete)).start()
+    return Thread(target=get_citation, args=(ids, dois, complete)).start()
 
 
 
 prev_time = time.time()
-
-
-
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
