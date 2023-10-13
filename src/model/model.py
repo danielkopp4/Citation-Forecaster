a48315e4a8bf476e84b458c30a960248e97a4f9f
Aksshat Gupta <aksshatyashgupta@gmail.com>
2023-10-13 18:32:18 -0400

    Proposed Optimizations

--- src/model/model.py
+++ src/model/model.py
@@ -1,19 +1,20 @@
-from src.data_extraction.dataset_api import Dataset
-from tensorflow.keras import Model, Input
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Dense, LeakyReLU, Concatenate, Add, BatchNormalization
-from tensorflow.keras.regularizers import L2
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import MeanAbsoluteError
-from src.model.callback import TrainingCallback
 import os
 import json
-import pprint
 import tensorflow as tf
+from src.data_extraction.dataset_api import Dataset
+from src.model.callback import TrainingCallback
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+import logging
 
 class Forecaster:
-    def __init__(self, params: dict):
-        self._params = params
+    def __init__(self, config_file):
+        with open(config_file, 'r') as file:
+            self._params = json.load(file)
+
         self._datasets = self._load_datasets()
         self._model = self._create_model()
 
@@ -21,6 +22,7 @@ class Forecaster:
             self._load_model()
 
         self._compile_model()
+        self._logger = self._setup_logger()
 
     @property
     def batch_size(self) -> int:
@@ -31,8 +33,13 @@ class Forecaster:
         return self._params.get('data_path', '')
 
     @property
-    def keras_model(self) -> tf.keras.Model:
-        return self._model
+    def keras_model(self) -> Model:
+        return self._model    
+
+    def _setup_logger(self):
+        logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+        return logger
 
     def _load_datasets(self):
         datasets = {}
@@ -44,73 +51,40 @@ class Forecaster:
         self._output_shape = datasets['train'].output_shape()
         return datasets
 
-
-    def __dense_layer(self, units, x):
-        return Dense(units, kernel_regularizer=L2(1), kernel_initializer="he_uniform")(x)
-
-
-    def __relu(self, x):
-        return LeakyReLU(self._params['alpha'])(x)
-    
-    
-    def __batch_norm(self, x):
-        return BatchNormalization()(x)
-    
-    
-    def __residual_layer(self, units, x):
-        x_skp = self.__dense_layer(units, x)
-        # x_skp = self.__batch_norm(x_skp)
-        x_skp = self.__relu(x_skp)
-        return Concatenate()([x, x_skp])
-        
-
-    def __create_model(self):
+    def _create_model(self):
         inpt = Input(shape=self._inpt_shape)
         x = inpt
 
-        for dim in self._params['model_dim']:
-            x = self.__residual_layer(dim, x)
-        
+        for dim in self._params.get('model_dim', []):
+            x = Dense(dim, kernel_regularizer=tf.keras.regularizers.L2(1), kernel_initializer="he_uniform")(x)
+            x = tf.keras.layers.LeakyReLU(self._params.get('alpha', 0.01))(x)
+
         assert len(self._output_shape) == 1
-        x = self.__dense_layer(self._output_shape[0], x)
-        self._model = Model(inputs=inpt, outputs=x)
+        x = Dense(self._output_shape[0], kernel_regularizer=tf.keras.regularizers.L2(1), kernel_initializer="he_uniform")(x)
+        return Model(inputs=inpt, outputs=x)
 
-
-    def __load_model(self):
-        params_loc = os.path.join(self._params['prev_dir'], 'params.json')
-        model_loc = os.path.join(self._params['prev_dir'], self._params['prev_name'])
+    def _load_model(self):
+        params_loc = os.path.join(self._params.get('prev_dir', ''), 'params.json')
+        model_loc = os.path.join(self._params.get('prev_dir', ''), self._params.get('prev_name', ''))
 
         with open(params_loc, 'r') as file:
             old_params = json.load(file)
 
-        for inherited in self._params['inherit']:
-            self._params[inherited] = old_params[inherited]
+        for inherited in self._params.get('inherit', []):
+            self._params[inherited] = old_params.get(inherited, self._params.get(inherited, ''))
 
         self._model = load_model(model_loc, compile=False)
 
-
-    def __save_model(self, epoch=None):
-        if not os.path.exists(self._params['models_dir']):
-            os.mkdir(self._params['models_dir'])
-
-        model_dir = os.path.join(self._params['models_dir'], self._params['model_name'])
+    def _save_model(self, epoch=None):
+        model_dir = os.path.join(self._params.get('models_dir', ''), self._params.get('model_name', ''))
+        model_dir = os.path.join(model_dir, 'final' if epoch is None else 'checkpoint')
 
         if not os.path.exists(model_dir):
-            os.mkdir(model_dir)
-
-        if epoch == None:
-            # final
-            model_dir = os.path.join(model_dir, 'final')
-        else:
-            # checkpoint
-            model_dir = os.path.join(model_dir, 'checkpoint')
-
-        if not os.path.exists(model_dir):
-            os.mkdir(model_dir)
+            os.makedirs(model_dir)
 
         params_loc = os.path.join(model_dir, 'params.json')
         model_loc = os.path.join(model_dir, f'trained_model')
-        if epoch != None:
+        if epoch is not None:
             model_loc += f'_{epoch}'
 
         with open(params_loc, 'w') as file:
@@ -118,61 +92,52 @@ class Forecaster:
 
         self.keras_model.save(model_loc, include_optimizer=False)
 
-
-    def __compile_model(self):
+    def _compile_model(self):
         optimizer = Adam(
-            self._params['learning_rate'],
-            self._params['beta_1'],
-            self._params['beta_2']            
+            self._params.get('learning_rate', 0.001),
+            self._params.get('beta_1', 0.9),
+            self._params.get('beta_2', 0.999)
         )
 
         self.keras_model.compile(
             optimizer=optimizer,
             loss='mse',
-            metrics=[MeanAbsoluteError()]
+            metrics=[tf.keras.metrics.MeanAbsoluteError()]
         )
-
 
     def print_params(self):
-        print('model params:')
-        pprint.pprint(self._params)
-        print('model summary:')
+        self._logger.info('Model Parameters:')
+        self._logger.info(json.dumps(self._params, indent=4))
+        self._logger.info('Model Summary:')
         self.keras_model.summary()
-        print(end='', flush=True)
-        
 
     def train(self):
-        print(30*'=', '|', ' Starting Training ', '|',  30*'=', sep='')
-        self.keras_model.fit(
+        self._logger.info('Starting Training')
+        early_stopping = EarlyStopping(patience=self._params.get('early_stopping_patience', 10), monitor='val_loss', restore_best_weights=True)
+        checkpoint = ModelCheckpoint(self._params.get('checkpoint_path', 'checkpoints/model_{epoch:02d}.h5'), save_best_only=True)
+        tensorboard = TensorBoard(log_dir=self._params.get('log_dir', ''))
+
+        history = self.keras_model.fit(
             self._datasets['train'],
             batch_size=self.batch_size,
-            epochs=self._params['epochs'],
+            epochs=self._params.get('epochs', 10),
             validation_data=self._datasets['val'],
             verbose=0,
-            callbacks=[TrainingCallback(
-                self._params['log_dir'], 
-                self._params['model_name'], 
-                plot_freq=self._params['plot_freq'],
-                print_freq=self._params['print_freq'],
-                checkpoint_freq=self._params['checkpoint_freq'],
-                checkpoint_fn=lambda epoch: self.__save_model(epoch)
-            )]
+            callbacks=[early_stopping, checkpoint, tensorboard]
         )
 
-        self.__save_model()
+        self._save_model()
+        self._logger.info('Finished Training')
 
-        print(30*'=', '|', ' Finished Training ', '|',  30*'=', sep='')
         self.keras_model.evaluate(
             self._datasets['test'],
             verbose=0,
             callbacks=[TrainingCallback(
-                self._params['log_dir'], 
-                self._params['model_name']
+                self._params.get('log_dir', ''),
+                self._params.get('model_name', '')
             )]
         )
 
-
-
-def get_model(params: dict) -> Forecaster:
-    model = Forecaster(params)
+def get_model(config_file: str) -> Forecaster:
+    model = Forecaster(config_file)
     return model
