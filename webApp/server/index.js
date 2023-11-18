d42bcc0c995378bcf6b5000bff4d6fe7a74e7ff8
vabalaji1 <vabalaji1234@gmail.com>
2023-11-17 19:47:52 -0500

    Interactio with the home page using body parser

--- webApp/server/index.js
+++ webApp/server/index.js
@@ -1,17 +1,18 @@
-// server/index.js
-
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+const port = 5000;
 
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.post('/submit-text', (req, res) => {
+  console.log('Received text:', req.body.text);
+  res.json({ message: 'Received' });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-}); 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
